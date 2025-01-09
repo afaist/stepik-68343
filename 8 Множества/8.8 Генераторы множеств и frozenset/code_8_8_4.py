9f0f70275b1eb6c5bf4cf54bfcb80b86f1e08e9b
afaist <afaist@gmail.com>
2025-01-09 16:07:22 +0700

    Первая буква слова

---
+++ 8 Множества/8.8 Генераторы множеств и frozenset/code_8_8_4.py
@@ -0,0 +1,25 @@
+"""
+Используя генератор множеств, дополните приведенный ниже код так, чтобы
+получить множество, содержащее первую букву каждого слова (в нижнем
+регистре) списка words. Результат вывести на одной строке в алфавитном
+порядке, разделяя элементы одним символом пробела.
+"""
+
+words = [
+    "Plum",
+    "Grapefruit",
+    "apple",
+    "orange",
+    "pomegranate",
+    "Cranberry",
+    "lime",
+    "Lemon",
+    "grapes",
+    "persimmon",
+    "tangerine",
+    "Watermelon",
+    "currant",
+    "Almond",
+]
+
+print(*sorted({word[0].lower() for word in words}))
